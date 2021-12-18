4ed9aa6ba872f777a94ca83d1e17775b5a605d06
Serhii Pylypenko <jpee.dev@gmail.com>
2021-12-18 18:16:53 +0200

    develop feat(task09) implement background color changer

--- docs/js/task-09.js
+++ docs/js/task-09.js
@@ -1,3 +1,19 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const app = () => {
+  const elements = {
+    button: document.querySelector('.widget .change-color'),
+    span: document.querySelector('.widget .color'),
+    body: document.querySelector('body'),
+  };
+
+  elements.button.addEventListener('click', () => {
+    const color = getRandomHexColor();
+    elements.span.textContent = color;
+    elements.body.style.backgroundColor = color;
+  });
+};
+
+app();
