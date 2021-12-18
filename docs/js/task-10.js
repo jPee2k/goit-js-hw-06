2cd6dfe9217531795f0b1a9fff5ad26483d89778
Serhii Pylypenko <jpee.dev@gmail.com>
2021-12-18 19:21:27 +0200

    develop feat(box generator) implement task 10

--- docs/js/task-10.js
+++ docs/js/task-10.js
@@ -1,3 +1,61 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const app = () => {
+  const elements = {
+    input: document.querySelector('#controls [type="number"]'),
+    createButton: document.querySelector('#controls [data-create]'),
+    destroyButton: document.querySelector('#controls [data-destroy]'),
+    boxWrapper: document.querySelector('#boxes'),
+  };
+
+  const data = {
+    amount: 0,
+    size: 30,
+    boxes: [],
+
+    createBoxes () {
+      for (let i = 0; i < this.amount; i += 1) {
+        const box = document.createElement('div');
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.height = `${this.size}px`;
+        box.style.width = `${this.size}px`;
+        this.incrementSize();
+        this.boxes.push(box);
+      }
+      return this;
+    },
+
+    removeBoxes() {
+      this.boxes.length = 0;
+      this.resetSize();
+      return this;
+    },
+
+    renderBoxes ()  {
+      elements.boxWrapper.replaceChildren(...this.boxes);
+      return this;
+    },
+
+    incrementSize () {
+      this.size += 10;
+    },
+
+    resetSize () {
+      this.size = 30;
+    },
+  };
+
+  elements.input.addEventListener('input', (evt) => {
+    data.amount = parseInt(evt.target.value || 0, 10);
+  });
+  elements.createButton.addEventListener('click', () => {
+    data.createBoxes().renderBoxes();
+  });
+  elements.destroyButton.addEventListener('click', () => {
+    data.removeBoxes().renderBoxes();
+  });
+};
+
+app();
